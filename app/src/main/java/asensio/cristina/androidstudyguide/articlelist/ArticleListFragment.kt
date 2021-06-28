a9762a16ee7eee96fd0e094f2d2de2739b54793b
casensio83 <casemun3@gmail.com>
2021-06-28 21:06:55 +0200

    small refactor

--- app/src/main/java/asensio/cristina/androidstudyguide/articlelist/ArticleListFragment.kt
+++ app/src/main/java/asensio/cristina/androidstudyguide/articlelist/ArticleListFragment.kt
@@ -1,10 +1,11 @@
-package asensio.cristina.androidstudyguide
+package asensio.cristina.androidstudyguide.articlelist
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import asensio.cristina.androidstudyguide.R
 
 
 class ArticleListFragment : Fragment() {
