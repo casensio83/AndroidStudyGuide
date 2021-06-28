544ffeb6e04afa9eb0bb33f8a9a87c2a7bfa834b
casensio83 <casemun3@gmail.com>
2021-06-28 21:43:28 +0200

    Setup adapter to recyclerview

--- app/src/main/java/asensio/cristina/androidstudyguide/articlelist/ArticleListFragment.kt
+++ app/src/main/java/asensio/cristina/androidstudyguide/articlelist/ArticleListFragment.kt
@@ -1,21 +1,41 @@
 package asensio.cristina.androidstudyguide.articlelist
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import asensio.cristina.androidstudyguide.R
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import asensio.cristina.androidstudyguide.databinding.FragmentArticleListBinding
 
 
 class ArticleListFragment : Fragment() {
 
+    private lateinit var binding: FragmentArticleListBinding
+    private lateinit var adapter: ArticleAdapter
+
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_article_list, container, false)
+    ): View {
+        adapter = ArticleAdapter()
+
+        binding = FragmentArticleListBinding.inflate(inflater, container, false)
+
+        setupRecyclerView()
+
+        return binding.root
+    }
+
+    private fun setupRecyclerView() {
+        binding.articleList.adapter = adapter
+        binding.articleList.layoutManager = LinearLayoutManager(context)
+
+        val dividerItemDecoration = DividerItemDecoration(context, DividerItemDecoration.VERTICAL)
+
+        binding.articleList.addItemDecoration(dividerItemDecoration)
     }
 
 }
