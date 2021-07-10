cbc33c06c19ec1342d6f10fe5c7ea8108ff32ecf
casensio83 <casemun3@gmail.com>
2021-07-10 16:02:27 +0200

    Small cleanup

--- app/src/main/java/asensio/cristina/androidstudyguide/articlelist/ArticleListFragment.kt
+++ app/src/main/java/asensio/cristina/androidstudyguide/articlelist/ArticleListFragment.kt
@@ -3,7 +3,6 @@ package asensio.cristina.androidstudyguide.articlelist
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -36,12 +35,6 @@ class ArticleListFragment : Fragment(), ArticleClickListener {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        val articles = articleRepository.fetchArticles()
-
-        articles.forEach {
-            Log.d("article > ", it.title)
-        }
 
         adapter.articles = articleRepository.fetchArticles()
     }
